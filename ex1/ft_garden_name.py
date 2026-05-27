fbfc56661a00fe69613d7933be7e70866feba52b
tmagoudi <tmagoudi@f4r7s7.paris.42.school>
2026-05-27 18:29:57 +0200

    les 3 premiers exos dans dossier

---
+++ ex1/ft_garden_name.py
@@ -0,0 +1,10 @@
+def ft_garden_name():
+    print("Enter garden name: ", end = "")
+    char = input()
+    print("Garden: ", char)
+    print (char)
+    print("Status: Growing well!")
+"""
+if __name__ == '__main__':
+    ft_garden_name()
+"""
---
+++ ex2/ft_plot_area.py
@@ -0,0 +1,8 @@
+def ft_plot_area():
+    length = int(input("Enter length: "))
+    width = int(input("Enter width: "))
+    print("Plot area: ", length * width)
+"""
+if __name__ == '__main__':
+    ft_plot_area()
+"""
